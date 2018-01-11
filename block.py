79c8a594baca76e80c20e824ae153d5be240b2b8
yunqu leon liu <leon.liu@viscore.com>
2018-01-11 18:52:09 -0500

    Add a few features 1. Print the time stamp in human read mode instead of unix mode 2. Parse the ScriptSig into script and InputPubkey 3. Parse the ScriptPubkey into OP_CODE and pubkeyHash 4. Added a output.txt which is a one block sample
    To do so, added in a new opcode.y file
    
    	modified:   block.py
    	new file:   opcode.py
    	new file:   output.txt
    	modified:   sight.py

--- block.py
+++ block.py
@@ -1,4 +1,7 @@
 from blocktools import *
+from opcode import *
+from datetime import datetime
+import time
 
 class BlockHeader:
 	def __init__(self, blockchain):
@@ -12,9 +15,12 @@ class BlockHeader:
 		print "Version:\t %d" % self.version
 		print "Previous Hash\t %s" % hashStr(self.previousHash)
 		print "Merkle Root\t %s" % hashStr(self.merkleHash)
-		print "Time\t\t %s" % str(self.time)
-		print "Difficulty\t %8x" % self.bits
+		print "Time stamp\t "+ self.decodeTime(self.time)
+		print "Difficulty\t %d" % self.bits
 		print "Nonce\t\t %s" % self.nonce
+	def decodeTime(self, time):
+		utc_time = datetime.utcfromtimestamp(time)
+		return utc_time.strftime("%Y-%m-%d %H:%M:%S.%f+00:00 (UTC)")
 
 class Block:
 	def __init__(self, blockchain):
@@ -39,6 +45,7 @@ class Block:
 
 			for i in range(0, self.txCount):
 				tx = Tx(blockchain)
+				tx.seq = i 
 				self.Txs.append(tx)
 		else:
 			self.continueParsing = False
@@ -58,11 +65,10 @@ class Block:
 		blockchain.seek(curPos)
 
 		tempBlockSize = fileSize - curPos
-		print tempBlockSize
+#		print "tempBlockSize \t %d" % tempBlockSize
 		if tempBlockSize < size:
 			return False
 		return True
-
 
 	def setHeader(self, blockchain):
 		self.blockHeader = BlockHeader(blockchain)
@@ -78,12 +84,14 @@ class Block:
 		print "##### Tx Count: %d" % self.txCount
 		for t in self.Txs:
 			t.toString()
+		print "#### end of all %d transactins" % self.txCount
 
 class Tx:
 	def __init__(self, blockchain):
 		self.version = uint4(blockchain)
 		self.inCount = varint(blockchain)
 		self.inputs = []
+		self.seq = 1
 		for i in range(0, self.inCount):
 			input = txInput(blockchain)
 			self.inputs.append(input)
@@ -97,7 +105,7 @@ class Tx:
 		
 	def toString(self):
 		print ""
-		print "="*10 + " New Transaction " + "="*10
+		print "="*20 + " No. %s " %self.seq + "Transaction " + "="*20
 		print "Tx Version:\t %d" % self.version
 		print "Inputs:\t\t %d" % self.inCount
 		for i in self.inputs:
@@ -107,7 +115,6 @@ class Tx:
 		for o in self.outputs:
 			o.toString()
 		print "Lock Time:\t %d" % self.lockTime
-				
 
 class txInput:
 	def __init__(self, blockchain):
@@ -118,11 +125,32 @@ class txInput:
 		self.seqNo = uint4(blockchain)
 
 	def toString(self):
-		print "Previous Hash:\t %s" % hashStr(self.prevhash)
-		print "Tx Out Index:\t %8x" % self.txOutId
-		print "Script Length:\t %d" % self.scriptLen
-		print "Script Sig:\t %s" % hashStr(self.scriptSig)
-		print "Sequence:\t %8x" % self.seqNo
+		print "\tPrev. Tx Hash:\t %s" % hashStr(self.prevhash)
+		print "\tTx Out Index:\t %s" % self.decodeOutIdx(self.txOutId)
+		print "\tScript Length:\t %d" % self.scriptLen
+#		print "\tScriptSig:\t %s" % 
+		self.decodeScriptSig(self.scriptSig)
+		print "\tSequence:\t %8x" % self.seqNo
+	def decodeScriptSig(self,data):
+		hexstr = hashStr(data)
+		if 0xffffffff == self.txOutId: #Coinbase
+			return hexstr
+		scriptLen = int(hexstr[0:2],16)
+		scriptLen *= 2
+		script = hexstr[2:2+scriptLen] 
+		print "\tScript:\t\t " + script
+		if SIGHASH_ALL != int(hexstr[scriptLen:scriptLen+2],16): # should be 0x01
+			print "\t Script op_code is not SIGHASH_ALL"
+			return hexstr
+		else: 
+			pubkey = hexstr[2+scriptLen+2:2+scriptLen+2+66]
+			print " \tInPubkey:\t "  + pubkey
+#		return hexstr
+	def decodeOutIdx(self,idx):
+		s = ""
+		if(idx == 0xffffffff):
+			s = " Coinbase with special index"
+		return "%8x"%idx + s 
 		
 class txOutput:
 	def __init__(self, blockchain):	
@@ -131,6 +159,29 @@ class txOutput:
 		self.pubkey = blockchain.read(self.scriptLen)
 
 	def toString(self):
-		print "Value:\t\t %d" % self.value
-		print "Script Len:\t %d" % self.scriptLen
-		print "Pubkey:\t\t %s" % hashStr(self.pubkey)
+		print "\tValue:\t\t %d" % self.value + " Satoshi"
+		print "\tScript Len:\t %d" % self.scriptLen
+		print "\tScriptPubkey:\t %s" % self.decodeScriptPubkey(self.pubkey)
+	def decodeScriptPubkey(self,data):
+		hexstr = hashStr(data)
+		op_code1 = OPCODE_NAMES[int(hexstr[0:2],16)]
+		if op_code1 == "OP_DUP":  #P2PKHA pay to pubkey hash mode
+	 		op_code2 = OPCODE_NAMES[int(hexstr[2:4],16)] + " "
+			keylen = int(hexstr[4:6],16) 
+			op_codeTail2nd = OPCODE_NAMES[int(hexstr[6+keylen*2:6+keylen*2+2],16)]
+			op_codeTailLast = OPCODE_NAMES[int(hexstr[6+keylen*2+2:6+keylen*2+4],16)]
+			print " \tPubkey OP_CODE:\t " + op_code1 + " " + op_code2 + " " + "Bytes:%d " % keylen +\
+					"tail_op_code:" +  op_codeTail2nd + " " + op_codeTailLast
+			print "\tPubkeyHash:\t       %s" % hexstr[6:6+keylen*2]
+			return hexstr	
+		elif op_code1 == "OP_HASH160": #P2SHA pay to script hash 
+			keylen = int(hexstr[2:4],16) 
+			op_codeTail = OPCODE_NAMES[int(hexstr[4+keylen*2:4+keylen*2+2],16)]
+			print " \tPubkey OP_CODE:\t " + op_code1 + " " + " " + "Bytes:%d " % keylen +\
+					"tail_op_code:" +  op_codeTail + " " 
+			print "\tPure Pubkey:\t     %s" % hexstr[4:4+keylen*2]
+			return hexstr
+		else: #TODO extend for multi-signature parsing 
+			print "\t Need to extend multi-signatuer parsing %x" % int(hexstr[0:2],16) + op_code1
+			return hexstr
+		
--- sight.py
+++ sight.py
@@ -3,8 +3,8 @@ import sys
 from blocktools import *
 from block import Block, BlockHeader
 
-def parse(blockchain):
-	print 'print Parsing Block Chain'
+def parse(blockchain, blkNo):
+	print 'Parsing Block Chain block head, transaction etc.'
 	continueParsing = True
 	counter = 0
 	blockchain.seek(0, 2)
@@ -16,17 +16,25 @@ def parse(blockchain):
 		if continueParsing:
 			block.toString()
 		counter+=1
+		print "#"*20+"Block counter No. %s"%counter +"#"*20
+		if counter >= blkNo and blkNo != 0xFF:
+			continueParsing = False
 
 	print ''
 	print 'Reached End of Field'
-	print 'Parsed %s blocks', counter
+	print "Parsed %d blocks" % counter
 
 def main():
 	if len(sys.argv) < 2:
             print 'Usage: sight.py filename'
 	else:
+		blkNo = 0xFF
+		if(len(sys.argv) == 3):
+			blkNo = int(sys.argv[2])
+			print "Parsing %d blocks" % blkNo
+
 		with open(sys.argv[1], 'rb') as blockchain:
-			parse(blockchain)
+			parse(blockchain,blkNo)
 
 
 
